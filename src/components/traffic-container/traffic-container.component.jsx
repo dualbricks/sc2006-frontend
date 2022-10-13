8237583f9b1e3c875965be2dcc48b7fadca71d6b
ongkangrui <ongkangrui@gmail.com>
2022-10-13 20:00:56 +0800

    implemented menu component and traffic container logout and post request for menu component logout needs to be looked at

---
+++ src/components/traffic-container/traffic-container.component.jsx
@@ -0,0 +1,11 @@
+// container for single traffic image
+
+const TrafficContainer = (trafficimage) => {
+  const { ImageLink, Name } = trafficimage;
+  return (
+    <div className="trafficcontainer">
+      <img src={ImageLink} />
+      <p>{Name}</p>
+    </div>
+  );
+};
